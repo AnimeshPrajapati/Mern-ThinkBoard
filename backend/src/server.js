5cca88e828618f80209b236fc199844770eabefd
Animesh Prajpati <animesh202p@gmail.com>
2025-10-10 15:40:19 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 //const express = require("express");{used for common js module type}
 
@@ -11,17 +12,19 @@ dotenv.config();
 
 const app = express () ;
 const PORT = process.env.PORT || 5001 ;
-
+const __dirname = path.resolve();
 
 
 //console.log(process.env.MONGO_URI); //to see the value of mongodb password
 
 //MIDDLE WARE
-app.use(
-  cors({
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
     origin:"http://localhost:5173",
   })
 );
+}
 app.use(express.json()); // this middle ware will parse json bodies:req.body
 app.use(rateLimiter);
 
@@ -32,6 +35,14 @@ app.use(rateLimiter);
 //});
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+ });
+}
 
 connectDB().then(() => {
   app.listen(5001, () => {
